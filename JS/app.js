d12da180fd6fef8289e34407d57e3bdf0f474fba
honggoun <fangri@naver.com>
2024-11-07 13:28:34 +0900

    LOGIN(2)
    LOGIN(2)

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDE_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDE_CLASSNAME);
+    console.log(username)
+    greeting.innerText = "hello" + username
+    greeting.classList.remove(HIDDE_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
