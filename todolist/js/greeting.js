35f5f9656f20f997402a8af43b22114aea4027c0
honggoun <fangri@naver.com>
2024-11-07 23:04:18 +0900

    3
    clock 2

---
+++ todolist/js/greeting.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDE_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDE_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username)
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDE_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDE_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername)
+}
